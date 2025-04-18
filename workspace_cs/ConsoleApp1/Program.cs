7e8a9579bd5bb050f2de4d2f231e7ddea31b3c85
Sme1503 <500cc6150@naver.com>
2025-04-18 17:58:50 +0900

    2025 04 18 c# 파일 업로드

---
+++ workspace_cs/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace kr.or.human
+{
+
+
+    struct Member
+    {
+        public string name;
+        public int age;
+    }
+    class Program()
+        {
+
+        Program()
+        {
+
+        }
+
+        // 소멸자
+        ~Program()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 2, 3, 4, 5, };
+            Console.WriteLine("Hello World");
+        }
+
+    }
+        
+}
